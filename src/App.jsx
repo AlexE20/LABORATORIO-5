cd97b99c970491262d9699909e14f8f0b172ee06
AlexE20 <00009423@uca.edu.sv>
2024-10-03 10:53:43 -0600

    Uploading files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Home from "./page/Home";
+import "./App.css";
+function App() {
+return (
+<div className="style-container">
+{<Home />}
+</div>
+);
+}
+export default App;
---
+++ src/page/Home.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Header from "../components/header/Header";
+import ViewTasks from "../components/viewTask/container/ViewTasks";
+import "./Home.css";
+
+const Home = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (taskTitle) => {
+    setTasks([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isCompleted: false,
+      },
+    ]);
+  };
+
+  const toggleTaskCompletedById = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  };
+
+  const deleteTaskById = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="home-container">
+      {<Header handleAddTask={addTask} />}
+      <Header handleAddTask={addTask} />
+
+      {}
+      <ViewTasks
+        tasks={tasks}
+        deleteTaskById={deleteTaskById}
+        onCompletedTask={toggleTaskCompletedById}
+      />
+    </div>
+  );
+};
+
+export default Home;
